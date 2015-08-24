1c1e9506efedb7c1861dc03033e47488d314ba91
Leslie <lpoole26@gmail.com>
2015-08-24 16:03:42 -0700

    add second category

---
+++ src/main/java/Category.java
@@ -0,0 +1,52 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Category {
+  private int id;
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+  public String getName() {
+    return name;
+  }
+  public Category(String name) {
+    this.name =name;
+  }
+  public static List<Category> all() {
+    String sql = "SELECT id, name FROM categories";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Category.class);
+    }
+  }
+  @Override
+  public boolean equals(Object otherCategory){
+    if (!(otherCategory instanceof Category)) {
+      return false;
+    } else {
+      Category newCategory = (Category) otherCategory;
+      return this.getName().equals(newCategory.getName());
+    }
+  }
+
+  public void save() {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "INSERT INTO categories(name) VALUES (:name)";
+     this.id = (int) con.createQuery(sql, true)
+       .addParameter("name", this.name)
+       .executeUpdate()
+       .getKey();
+     }
+   }
+
+    public static Category find(int id) {
+       try(Connection con = DB.sql2o.open()) {
+         String sql = "SELECT * FROM categories where id=:id";
+         Category Category = con.createQuery(sql)
+           .addParameter("id", id)
+           .executeAndFetchFirst(Category.class);
+         return Category;
+       }
+     }
+}
